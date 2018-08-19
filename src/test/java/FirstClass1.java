d581279ef30fe5115a0888eacf0195dd8519e79f
BIMLESH <bpathak@digitalriver.com>
2018-08-19 18:18:50 +0530

    second commit

--- src/test/java/FirstClass1.java
+++ src/test/java/FirstClass1.java
@@ -1,9 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class FirstClass1 {
 
 	@Test
-	public void openBrowser(){
+	public void openBrowser() throws InterruptedException{
 		
+	WebDriver driver = new FirefoxDriver();
+	
+	driver.get("https://www.google.co.in/");
+	Thread.sleep(5000);
 	}
 }
